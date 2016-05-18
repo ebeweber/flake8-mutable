ee7d6ca5c41dea1d825543d24e9a19862da05c0c
Matthew Ebeweber <matthew@claralabs.com>
2016-05-17 20:39:36 -0700

    s/flake-mutable-defaults/flake8-mutable, setup.py cleanup

--- setup.py
+++ setup.py
@@ -3,17 +3,18 @@ import setuptools
 
 
 setuptools.setup(
-    version='1.0.0',
-    name='flake-mutable-defaults',
+    version='1.0.2',
+    name='flake8-mutable',
     description='mutable defaults flake8 extension',
     keywords='flake8 mutable arg kwarg',
     author='Matthew Ebeweber',
     author_email='mebeweber@gmail.com',
-    url='https://github.com/ebeweber/flake8-mutable-defaults',
+    url='https://github.com/ebeweber/flake8-mutable',
     license='MIT',
+    py_modules=['mutable_defaults'],
+    zip_safe=False,
     install_requires=[
         'flake8',
-        'setuptools',
     ],
     entry_points={
         'flake8.extension': [
